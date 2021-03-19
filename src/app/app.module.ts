85e3d649ed93d2d683b7f5972518cad830af17f3
Andrew Vitrenko <57104862+AndrewVitrenko@users.noreply.github.com>
2021-03-19 20:10:32 +0200

    new homework

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SliderComponent } from './components/slider/slider.component';
+import { SliderItemComponent } from './components/slider-item/slider-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SliderComponent,
+    SliderItemComponent
   ],
   imports: [
     BrowserModule,
