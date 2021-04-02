15b1ba5ccc7b2630f7ae920d9b0e2f51baeee6c8
Andrew Vitrenko <57104862+AndrewVitrenko@users.noreply.github.com>
2021-04-02 18:17:02 +0300

    fix lint troubles

--- src/app/components/slider/slider.component.ts
+++ src/app/components/slider/slider.component.ts
@@ -6,78 +6,68 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./slider.component.scss'],
 })
 export class SliderComponent implements OnInit {
-  context: this;
-  title = 'something';
   currentSlideIndex = 0;
-  slides: [string] = [''];
+  slides: string[] = [''];
   startPosition: number;
   lastTranslate: number;
-  bodyWidth: number = document.body.offsetWidth;
+  bodyWidth: number = document.body.offsetWidth - 17;
   translate: number = -0.189 * this.bodyWidth * this.currentSlideIndex;
   isActive = false;
+  sliderTrackWidth: number;
 
-  // tslint:disable-next-line:typedef
-  ngOnInit() {
+  ngOnInit(): void {
+    const track = document.querySelector('.slider__track') as HTMLElement;
+    this.sliderTrackWidth = track.offsetWidth;
     for (let i = 0; i < 13; i++) {
       if (i < 9) {
         this.slides[i] = `assets/images/img_0${i + 1}.jpg`;
-      }
-      else {
+      } else {
         this.slides[i] = `assets/images/img_${i + 1}.jpg`;
       }
     }
   }
 
-  // tslint:disable-next-line:typedef
-  next() {
+  next(): void {
     if (this.currentSlideIndex < 9) {
       this.currentSlideIndex++;
     }
-
-    // tslint:disable-next-line:max-line-length
-    this.translate = -0.189 * this.bodyWidth * this.currentSlideIndex < -1.526 * this.bodyWidth ? -1.526 * this.bodyWidth : -0.189 * this.currentSlideIndex * this.bodyWidth;
+    if (-0.189 * this.bodyWidth * this.currentSlideIndex < 0.9 * this.bodyWidth - this.sliderTrackWidth) {
+      this.translate = 0.9 * this.bodyWidth - this.sliderTrackWidth;
+    } else {
+      this.translate = -0.189 * this.currentSlideIndex * this.bodyWidth;
+    }
   }
 
-  // tslint:disable-next-line:typedef
-  previous() {
+  previous(): void {
     if (this.currentSlideIndex) {
       this.currentSlideIndex--;
     }
     this.translate = -0.189 * document.body.offsetWidth * this.currentSlideIndex;
   }
 
-  // tslint:disable-next-line:typedef
-  onDragStart(event: MouseEvent) {
+  onDragStart(event: MouseEvent): void {
     this.startPosition = event.clientX;
     this.lastTranslate = this.translate;
     this.isActive = true;
-    console.log('Start params:', this.lastTranslate, this.startPosition)
-    this.onDragMove(event)
   }
 
-  // tslint:disable-next-line:typedef
-  onDragMove(event: MouseEvent) {
+  onDragMove(event: MouseEvent): void {
     if (this.isActive) {
       const translate = this.lastTranslate + event.clientX - this.startPosition;
 
       if (translate > 0) {
         this.translate = 0;
-      }
-      else if (translate < -1.526 * this.bodyWidth) {
-        this.translate = -1.526 * this.bodyWidth;
-      }
-      else {
-        this.translate = translate
+      } else if (translate < 0.9 * this.bodyWidth - this.sliderTrackWidth) {
+        this.translate = 0.9 * this.bodyWidth - this.sliderTrackWidth;
+      } else {
+        this.translate = translate;
       }
     }
   }
 
-  // tslint:disable-next-line:typedef
-  onDragEnd(event: MouseEvent) {
-    this.onDragMove(event)
+  onDragEnd(): void {
     this.isActive = false;
     this.lastTranslate = this.translate;
-    this.currentSlideIndex = Math.round(this.translate / (-0.189) / this.bodyWidth)
-    console.log('End params:', this.lastTranslate, this.startPosition)
+    this.currentSlideIndex = Math.ceil(this.translate / (-0.189) / this.bodyWidth);
   }
 }
